af327eea59cc8d4060376b88e6c66e16337cb946
ct1559 <e.cooper.taylor@gmail.com>
2022-10-30 12:49:54 -0400

    Add HTML structure to home page

---
+++ home.php
@@ -0,0 +1,34 @@
+<?php
+  // Start a session
+  session_start();
+  // Redirect to login page if user is not logged in
+  if (!isset($_SESSION['loggedin'])) {
+    header('Location: index.html');
+    exit;
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home Page</title>
+  <link href="styles.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+</head>
+<body class="loggedin">
+  <nav class="navtop">
+    <div>
+      <h1>Website Title</h1>
+      <a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
+      <a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+    </div>
+  </nav>
+  <div class="content">
+    <h2>Home Page</h2>
+    <p>Welcome back, <?=$_SESSION['name']?>!</p>
+  </div>
+</body>
+</html>
